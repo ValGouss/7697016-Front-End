634ca588eb69a1a78c14518f39974a9dae7d820c
ValGouss <v.goussault@gmail.com>
2023-01-05 14:36:56 +0100

    modified:   pieces.js

--- pieces.js
+++ pieces.js
@@ -4,34 +4,34 @@ const pieces = await reponse.json();
 
 for (let i = 0; i < pieces.length; i++) {
 
-//Création des balises
-const article = pieces[i];
-const imageElement = document.createElement('img');
-imageElement.src = article.image;
+    //Création des balises
+    const article = pieces[i];
+    const imageElement = document.createElement('img');
+    imageElement.src = article.image;
 
-const nomElement = document.createElement('h2');
-nomElement.innerText = article.nom;
+    const nomElement = document.createElement('h2');
+    nomElement.innerText = article.nom;
 
-const prixElement = document.createElement('p');
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const prixElement = document.createElement('p');
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 
-const categorieElement = document.createElement('p');
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const categorieElement = document.createElement('p');
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
-const descriptionElement = document.createElement('p');
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
+    const descriptionElement = document.createElement('p');
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
 
-const stockElement = document.createElement('p');
-stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    const stockElement = document.createElement('p');
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
 
 
-//Ratachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+    //Ratachement de nos balises au DOM
+    const sectionFiches = document.querySelector(".fiches");
+    sectionFiches.appendChild(imageElement);
+    sectionFiches.appendChild(nomElement);
+    sectionFiches.appendChild(prixElement);
+    sectionFiches.appendChild(categorieElement);
+    sectionFiches.appendChild(descriptionElement);
+    sectionFiches.appendChild(stockElement);
 
 }
